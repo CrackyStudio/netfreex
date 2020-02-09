a9fbb29811693c90ce91c57abca4bad4f5a70839
Antoine <antoine.nivoy@gmail.com>
2020-02-09 11:03:49 +0100

    Add fetch

---
+++ app/src/services/fetch.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const API = `http://${process.env.REACT_APP_SERVER_IP}:4242/` as string;
+
+const get = async (url: string): Promise<any> => {
+	try {
+		const response = await fetch(url, {
+			method: 'GET',
+		});
+		return response.json();
+	} catch (errors) {
+		console.log(API)
+		console.log(errors);
+		throw errors;
+	}
+};
+
+export default {
+	getFilms: (): any => get(`${API}films`),
+};
