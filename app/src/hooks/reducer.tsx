9fdef8b100dc5aba4f1ea8af233fe7f53279e3b0
Antoine <antoine.nivoy@gmail.com>
2020-02-09 11:04:27 +0100

    Add hooks

---
+++ app/src/hooks/reducer.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const Reducer = (state: any, action: any): any => {
+	return {
+		...state,
+		[action.key]: action.value,
+	}
+};
