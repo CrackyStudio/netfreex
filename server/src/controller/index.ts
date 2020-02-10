4e2f0b04b7b9203e9e4cee3514005bac8441f75a
Antoine <antoine.nivoy@gmail.com>
2020-02-10 16:53:32 +0100

    Add series methods

--- server/src/controller/index.ts
+++ server/src/controller/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { resolve } from "path";
-import { readdirSync } from 'fs';
+import { resolve, join } from "path";
+import { readdirSync, statSync } from 'fs';
 
 export class Controller {
     public index(_req: Request, res: Response) {
@@ -19,7 +19,7 @@ export class Controller {
         });
     }
 
-    public async getFilm(req: Request, res: Response) {
+    public getFilm(req: Request, res: Response) {
         const film: string = req.params.film;
         const file = `${resolve(__dirname, `../../public/videos/films/${film}.mp4`)}`
         res.download(file);
@@ -32,6 +32,45 @@ export class Controller {
     }
     
     public series(_req: Request, res: Response) {
-        // toDo
+        const path = `${resolve(__dirname, "../../public/videos/series")}`;
+        const directories = readdirSync(path).filter(f => statSync(join(path, f)).isDirectory())
+        res.json({
+            Series: directories
+        });
+    }
+
+    public getSerieImage(req: Request, res: Response) {
+        const image: string = req.params.image;
+        const file = `${resolve(__dirname, `../../public/images/series/${image}.jpg`)}`
+        res.download(file);
+    }
+
+    public getSeasons(req: Request, res: Response) {
+        const serie: string = req.params.serie;
+        const path = `${resolve(__dirname, `../../public/videos/series/${serie}`)}`;
+        const seasons = readdirSync(path).filter(f => statSync(join(path, f)).isDirectory())
+        res.json({
+            Seasons: seasons
+        });
+    }
+
+    public getEpisodes(req: Request, res: Response) {
+        const serie: string = req.params.serie;
+        const season: string = req.params.season
+        const files = readdirSync(`${resolve(__dirname, `../../public/videos/series/${serie}/${season}`)}`);
+        files.forEach((episode, i) => {
+            files[i] = episode.replace(/\.[^/.]+$/, "")
+        })
+        res.json({
+            Episodes: files
+        });
+    }
+
+    public getEpisode(req: Request, res: Response) {
+        const serie: string = req.params.serie;
+        const season: string = req.params.season;
+        const episode: string = req.params.episode;
+        const file = `${resolve(__dirname, `../../public/videos/series/${serie}/${season}/${episode}.mp4`)}`
+        res.download(file);
     }
 }
