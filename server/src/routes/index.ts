b60703f72bb730646c61fc76f1ab3014860105a5
Antoine <antoine.nivoy@gmail.com>
2020-02-10 16:53:39 +0100

    Add series routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -10,6 +10,10 @@ export class Routes {
         app.route("/films/:film").get(this.Controller.getFilm);
         app.route("/films/images/:image").get(this.Controller.getFilmImage)
 
-        app.route("/series").get(this.Controller.series);        
+        app.route("/series").get(this.Controller.series); 
+        app.route("/series/:serie").get(this.Controller.getSeasons);
+        app.route("/series/:serie/:season").get(this.Controller.getEpisodes);
+        app.route("/series/get/images/:image").get(this.Controller.getSerieImage);
+        app.route("/series/:serie/:season/:episode/play").get(this.Controller.getEpisode);      
     }
 }
