1c2c4df28bc0bd79d1c3d071b44987ee924ef289
Antoine <antoine.nivoy@gmail.com>
2020-02-17 10:47:56 +0100

    Reference style path

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
+import './styles/app/index.css';
 import App from './screens/app';
 import * as serviceWorker from './serviceWorker';
 
