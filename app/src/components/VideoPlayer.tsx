944eadf57210d3fb24c05276130fa429cdeea885
Antoine <antoine.nivoy@gmail.com>
2020-02-19 14:59:37 +0100

    Add download functionality

--- app/src/components/VideoPlayer.tsx
+++ app/src/components/VideoPlayer.tsx
@@ -2,21 +2,34 @@ import 'styles/components/backButton.css'
 import React from 'react';
 import ReactPlayer from 'react-player';
 import BackButton from 'components/BackButton';
+import { Button } from 'evergreen-ui';
 import 'styles/components/videoPlayer.css';
 
 const VideoPlayer = ({source, backButton = true}: {source: string, backButton?: boolean}) => {
+
+    const downloadFile = () => {
+        window.open(source);
+    }
+
 	return (
         <div className="player-container">
-           <div className="player">
-            <ReactPlayer playing={true} controls={true} url={source} pip={true} height='440px' config={{
-                    file: {
-                        attributes: { autoPictureInPicture: true }
-                    },
-                }}/>
-           </div>
-            {backButton && (
-                <BackButton screen={"player"} text="Revenir au catalogue"/>
-            )}
+            <div className="player">
+                <ReactPlayer playing={true} controls={true} url={source} pip={true} width="640px" height='440px' config={{
+                        file: {
+                            attributes: { autoPictureInPicture: true }
+                        },
+                    }}/>
+            </div>
+            <div className="player-buttons">
+                <div className="goback-container">
+                    {backButton && (
+                        <BackButton screen={"player"} text="Revenir au catalogue"/>
+                    )}
+                </div>
+                <div className="download-container">
+                    <Button className="download" appearance="primary" intent="danger" iconBefore="download" onClick={() => downloadFile()}>Télécharger</Button>
+                </div>
+            </div>
         </div>
 	);
 };
