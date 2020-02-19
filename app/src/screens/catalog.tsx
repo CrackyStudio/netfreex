8ad85499f837036d7610ee9c7a6b3cb3317e53f5
Antoine <antoine.nivoy@gmail.com>
2020-02-19 14:07:38 +0100

    Use custom button & component

--- app/src/screens/catalog.tsx
+++ app/src/screens/catalog.tsx
@@ -1,30 +1,26 @@
 import React from 'react';
 import Films from "screens/films";
 import Series from 'screens/series';
+import mLogo from  "media/popcorn.gif";
 import { useStateValue } from 'hooks/provider';
-import { Button } from 'evergreen-ui';
 import 'styles/screens/catalog.css';
 
 const Catalog: React.FC = () => {
-    const [{ videoType }, dispatch] = useStateValue() as Array<any>;
-
-    const setVideoType = (videoType: string) => {
-        dispatch({ key: 'videoType', value: videoType });
-    }
+    const [{ category }] = useStateValue() as Array<any>;
 
 	return (
         <> 
-            {!videoType &&  (
-                <div className="video-type-container">
-                    <Button appearance="primary" intent="danger" margin={6} onClick={() => setVideoType("Films")}>Films</Button>
-                    <Button appearance="primary" intent="danger" margin={6} onClick={() => setVideoType("Series")}>Series</Button>
-                </div>
-            )}
-            {videoType === "Films" && (
+            {category === "Films" && (
                 <Films/>
             )}
-            {videoType === "Series" && (
+            {category === "Series" && (
                 <Series/>
+            )}
+            {category === "" && (
+                <div className="popcorn-container">
+                    <img className="popcorn" src={mLogo} alt="popcorn"/>
+                    <p className="what-text">Alors, on fait quoi ?</p>
+                </div>
             )}
         </>
     )
--- app/src/screens/dashboard.tsx
+++ app/src/screens/dashboard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Logo from  "media/netfreex.png";
+import mLogo from  "media/moshed-logo.gif";
 import Catalog from 'screens/catalog';
 import Login from 'screens/login';
+import HeaderButton from 'components/HeaderButton';
 import { useStateValue } from 'hooks/provider';
 import 'styles/screens/dashboard.css';
 
@@ -10,17 +12,29 @@ const Dashboard: React.FC = () => {
 
     const goHome = () => {
         dispatch({ key: 'showPlayer', value: false });
-        dispatch({ key: 'videoType', value: "" });
-        dispatch({ key: 'episodes', value: [] });
-        dispatch({ key: 'seasons', value: "" });
-        dispatch({ key: 'serie', value: "" });
+        dispatch({ key: 'category', value: "" });
         dispatch({ key: 'film', value: "" });
+        dispatch({ key: 'serie', value: "" });
+        dispatch({ key: 'seasons', value: "" });
+        dispatch({ key: 'season', value: "" });
+        dispatch({ key: 'seasonIndex', value: "" });
+        dispatch({ key: 'episodes', value: [] });
+        dispatch({ key: 'episode', value: [] });
+        dispatch({ key: 'episodeIndex', value: [] });
     }
-
+    
 	return (
         <div className="main">
-            <div className="header">
-                <img className="logo" src={Logo} alt="netfreex-logo" onClick={() => goHome()}/>
+            <div className={isLogged ? "header" : "header-centered"}>
+                <img className="logo" src={mLogo} alt="netfreex-logo" onClick={() => goHome()}/>
+                { isLogged && (
+                    <>
+                        <HeaderButton text="Films"/>
+                        <HeaderButton text="Series"/>
+                        <HeaderButton text="Musiques" enabled={false}/>
+                        <HeaderButton text="Apps" enabled ={false}/>
+                    </>
+                )}
             </div>
             {!isLogged ? <Login/> : <Catalog/>}
         </div>
