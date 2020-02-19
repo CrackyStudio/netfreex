2dafb298ac5426d6ff221afac606df196eaff0b3
Antoine <antoine.nivoy@gmail.com>
2020-02-19 11:41:24 +0100

    Add player background

--- app/src/components/VideoPlayer.tsx
+++ app/src/components/VideoPlayer.tsx
@@ -7,11 +7,13 @@ import 'styles/components/videoPlayer.css';
 const VideoPlayer = ({source, backButton = true}: {source: string, backButton?: boolean}) => {
 	return (
         <div className="player-container">
+           <div className="player">
             <ReactPlayer playing={true} controls={true} url={source} pip={true} height='440px' config={{
-                file: {
-                    attributes: { autoPictureInPicture: true }
-                },
-            }}/>
+                    file: {
+                        attributes: { autoPictureInPicture: true }
+                    },
+                }}/>
+           </div>
             {backButton && (
                 <BackButton screen={"player"} text="Revenir au catalogue"/>
             )}
