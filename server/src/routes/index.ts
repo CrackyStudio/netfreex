fcefa61e9b5a045f9b85c22c20442e3a8458b4b1
CrackyStudio <antoine.nivoy@gmail.com>
2020-02-25 12:41:16 +0100

    Update routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,19 +1,30 @@
-import { Controller } from "../controller";
+import { RootController } from "../controller/rootController";
+import { UserController } from "controller/userController";
+import { VideoController } from "../controller/videoController";
 
 export class Routes {
-	public Controller: Controller = new Controller();
+	public rootController: RootController = new RootController();
+	public userController: UserController = new UserController();
+	public videoController: VideoController = new VideoController();
 
 	public routes(app: any): void {
-		app.route("/").get(this.Controller.index);
+		// Root routes
+		app.route("/").get(this.rootController.index);
 
-		app.route("/films").get(this.Controller.films);
-		app.route("/films/:film").get(this.Controller.getFilm);
-		app.route("/films/images/:image").get(this.Controller.getFilmImage);
+		// User routes
+		/* TODO */
 
-		app.route("/series").get(this.Controller.series);
-		app.route("/series/:serie").get(this.Controller.getSeasons);
-		app.route("/series/:serie/:season").get(this.Controller.getEpisodes);
-		app.route("/series/get/images/:image").get(this.Controller.getSerieImage);
-		app.route("/series/:serie/:season/:episode/play").get(this.Controller.getEpisode);
+		// Video routes
+		app.route("/films").get(this.videoController.films);
+		app.route("/films/:film").get(this.videoController.getFilm);
+		app.route("/films/images/:image").get(this.videoController.getFilmImage);
+
+		app.route("/series").get(this.videoController.series);
+		app.route("/series/:serie").get(this.videoController.getSeasons);
+		app.route("/series/:serie/:season").get(this.videoController.getEpisodes);
+		app.route("/series/get/images/:image").get(this.videoController.getSerieImage);
+		app.route("/series/:serie/:season/:episode/play").get(this.videoController.getEpisode);
+		
+		// REFACTO: Split videos routes by 2 controllers filmController & serieController
 	}
 }
