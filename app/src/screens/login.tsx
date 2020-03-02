d4f0938ed04a69d7c5f5d3c6cccf5f53ac35a843
CrackyStudio <antoine.nivoy@gmail.com>
2020-03-02 13:28:17 +0100

    Fix errors

--- app/src/screens/login.tsx
+++ app/src/screens/login.tsx
@@ -10,10 +10,24 @@ const Login: React.FC = () => {
 	const [{ user, passcode, register, secret }, dispatch] = useStateValue() as Array<any>;
 
 	useEffect(() => {
+		const verify2FA = () => {
+			fetch.validate2FA({ token: passcode, secret }).then((response: any) => {
+				if (response === "2FA authorization succeded") {
+					dispatch({ key: "isLogged", value: true });
+				} else {
+					dispatch({ key: "secret", value: undefined });
+					toaster.danger("L'authentification a échoué", {
+						description: "Le code saisi est incorrect ou n'est plus valide (30s)",
+						duration: 5
+					});
+				}
+			});
+		}
+
         if (secret !== undefined) {
 			verify2FA()
 		}
-	}, [secret]);
+	}, [secret, passcode, dispatch]);
 	
 	const setUser = (e: any) => {
 		dispatch({ key: "user", value: e.target.value });
@@ -23,19 +37,6 @@ const Login: React.FC = () => {
 		dispatch({ key: "register", value: true});
 	}
 
-	const verify2FA = () => {
-        fetch.validate2FA({ token: passcode, secret }).then((response: any) => {
-            if (response === "2FA authorization succeded") {
-                dispatch({ key: "isLogged", value: true });
-            } else {
-                toaster.danger("L'authentification a échoué", {
-					description: "Le code saisi est incorrect ou n'est plus valide (30s)",
-					duration: 5
-				});
-            }
-		});
-	}
-	
 	const login = async () => {
 		dispatch({ key: "secret", value: undefined });
 		if (passcode !== undefined && passcode.length === 6) {
